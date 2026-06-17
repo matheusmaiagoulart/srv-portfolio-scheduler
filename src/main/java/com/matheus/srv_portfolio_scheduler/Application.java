4dfb3e8fb63fedc5978e5a06e0e008e43c63542f
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-06-17 17:37:10 -0300

    feat: init project repository

---
+++ src/main/java/com/matheus/srv_portfolio_scheduler/Application.java
@@ -0,0 +1,13 @@
+package com.matheus.srv_portfolio_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/srv_portfolio_scheduler/SrvPortfolioSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.srv_portfolio_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvPortfolioSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
