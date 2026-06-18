c27b774e4db328120124e95f94ceb93c2c7b2245
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-06-18 11:37:48 -0300

    refactor: remove unused import

--- src/main/java/com/matheus/srv_portfolio_scheduler/infrastructure/persistence/JpaRecommendedPortfolioRepository.java
+++ src/main/java/com/matheus/srv_portfolio_scheduler/infrastructure/persistence/JpaRecommendedPortfolioRepository.java
@@ -1,6 +1,5 @@
 package com.matheus.srv_portfolio_scheduler.infrastructure.persistence;
 
-import com.matheus.srv_portfolio_scheduler.domain.entities.RecommendedPortfolio;
 import com.matheus.srv_portfolio_scheduler.infrastructure.entities.JpaRecommendedPortfolio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
