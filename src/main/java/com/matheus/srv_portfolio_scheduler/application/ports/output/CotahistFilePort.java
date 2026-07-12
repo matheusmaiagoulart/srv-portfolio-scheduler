3d0b162f6079fc6767047659a685aa68b579b97c
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-07-12 20:37:46 -0300

    feat: add CotahistFilePort interface and CotahistFileAdapter implementation; introduce CotahistNotFoundException

---
+++ src/main/java/com/matheus/srv_portfolio_scheduler/application/ports/output/CotahistFilePort.java
@@ -0,0 +1,15 @@
+package com.matheus.srv_portfolio_scheduler.application.ports.output;
+
+import com.matheus.srv_portfolio_scheduler.application.dto.QuoteDTO;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+
+public interface CotahistFilePort {
+    List<QuoteDTO> parse (HashSet<String> tickers, String pathFile);
+    List<QuoteDTO> getTickerByLastCotahist (HashSet<String> tickers);
+    Optional<String> existsCotahistFile (LocalDate referenceDate);
+}
