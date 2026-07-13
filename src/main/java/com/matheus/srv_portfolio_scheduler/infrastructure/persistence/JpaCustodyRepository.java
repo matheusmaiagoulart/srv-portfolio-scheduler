8c75984a79486ea1001b8b31dcb9d77274b1119e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-07-13 19:50:22 -0300

    refactor: remove unused import for Custody entity in JpaCustodyRepository

--- src/main/java/com/matheus/srv_portfolio_scheduler/infrastructure/persistence/JpaCustodyRepository.java
+++ src/main/java/com/matheus/srv_portfolio_scheduler/infrastructure/persistence/JpaCustodyRepository.java
@@ -1,6 +1,5 @@
 package com.matheus.srv_portfolio_scheduler.infrastructure.persistence;
 
-import com.matheus.srv_portfolio_scheduler.domain.entities.Custody;
 import com.matheus.srv_portfolio_scheduler.infrastructure.entities.JpaCustody;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
