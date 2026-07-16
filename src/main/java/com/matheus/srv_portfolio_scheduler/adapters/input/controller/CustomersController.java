00d19b7bf26ee9082115ec5336e713e3c053629e
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-07-16 14:07:19 -0300

    feat: add endpoint for retrieving customer portfolio and update registration endpoint

--- src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/controller/CustomersController.java
+++ src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/controller/CustomersController.java
@@ -3,26 +3,33 @@ package com.matheus.srv_portfolio_scheduler.adapters.input.controller;
 import com.matheus.srv_portfolio_scheduler.adapters.input.swagger.SwaggerCustomersController;
 import com.matheus.srv_portfolio_scheduler.application.command.RegisterCustomerSubscriber.RegisterCustomerSubscriberCommand;
 import com.matheus.srv_portfolio_scheduler.application.command.RegisterCustomerSubscriber.RegisterCustomerSubscriberResponse;
+import com.matheus.srv_portfolio_scheduler.application.ports.input.GetCustomerPortfolioUseCase;
 import com.matheus.srv_portfolio_scheduler.application.ports.input.RegisterCustomerSubscriberUseCase;
+import com.matheus.srv_portfolio_scheduler.application.queries.GetCustomerPortfolio.GetCustomerPortfolioQuery;
+import com.matheus.srv_portfolio_scheduler.application.queries.GetCustomerPortfolio.GetCustomerPortfolioResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping(value = "v1/api/customers")
 public class CustomersController implements SwaggerCustomersController {
 
+    private final GetCustomerPortfolioUseCase getCustomerPortfolioUseCase;
     private final RegisterCustomerSubscriberUseCase registerCustomerSubscriberUseCase;
 
     @Override
-    @PostMapping
+    @PostMapping("adesao")
     public ResponseEntity<RegisterCustomerSubscriberResponse> registerCustomerSubscriber(
             @RequestBody @Valid RegisterCustomerSubscriberCommand command) {
         return ResponseEntity.status(201).body(registerCustomerSubscriberUseCase.handler(command));
+    }
+
+    @GetMapping("{id}/carteira")
+    public ResponseEntity<GetCustomerPortfolioResponse> getCustomerPortfolio(@PathVariable long id) {
+
+        return ResponseEntity.ok(getCustomerPortfolioUseCase.handler(new GetCustomerPortfolioQuery(id)));
     }
 }
--- src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/swagger/SwaggerCustomersController.java
+++ src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/swagger/SwaggerCustomersController.java
@@ -2,12 +2,14 @@ package com.matheus.srv_portfolio_scheduler.adapters.input.swagger;
 
 import com.matheus.srv_portfolio_scheduler.application.command.RegisterCustomerSubscriber.RegisterCustomerSubscriberCommand;
 import com.matheus.srv_portfolio_scheduler.application.command.RegisterCustomerSubscriber.RegisterCustomerSubscriberResponse;
+import com.matheus.srv_portfolio_scheduler.application.queries.GetCustomerPortfolio.GetCustomerPortfolioResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "CustomersController")
@@ -15,7 +17,12 @@ public interface SwaggerCustomersController {
 
     @Operation(summary = "Criação do usuário",
             description = "Criação do usuário inscrito para participar da compra automatizada de ações da corretora.")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ok")})
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "Ok")})
     ResponseEntity<RegisterCustomerSubscriberResponse> registerCustomerSubscriber(
             @RequestBody @Valid RegisterCustomerSubscriberCommand command);
+
+    @Operation(summary = "Consulta de carteira",
+            description = "Consulta de carteira do cliente, com ativos, quantidades, lucros e evolução.")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Ok")})
+    ResponseEntity<GetCustomerPortfolioResponse> getCustomerPortfolio(@PathVariable long id);
 }
