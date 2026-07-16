25d61a90196f880be8cba5a2d70b7eb61e320f85
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-07-16 14:05:01 -0300

    feat: implement GetCustomerPortfolio query handler and related data structures

---
+++ src/main/java/com/matheus/srv_portfolio_scheduler/application/ports/input/GetCustomerPortfolioUseCase.java
@@ -0,0 +1,8 @@
+package com.matheus.srv_portfolio_scheduler.application.ports.input;
+
+import com.matheus.srv_portfolio_scheduler.application.queries.GetCustomerPortfolio.GetCustomerPortfolioQuery;
+import com.matheus.srv_portfolio_scheduler.application.queries.GetCustomerPortfolio.GetCustomerPortfolioResponse;
+
+public interface GetCustomerPortfolioUseCase {
+    GetCustomerPortfolioResponse handler(GetCustomerPortfolioQuery query);
+}
