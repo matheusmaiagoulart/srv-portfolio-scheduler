a73152794807974e79117ca26d66385cf40ce610
Matheus Maia Goulart <matheusmaiagoulart2004@gmail.com>
2026-08-30 17:00:29 -0300

    feat: improve Redis cache invalidation for customer portfolios and update execute purchase endpoint to handle request body

--- src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/controller/PurchasesController.java
+++ src/main/java/com/matheus/srv_portfolio_scheduler/adapters/input/controller/PurchasesController.java
@@ -4,12 +4,13 @@ import com.matheus.srv_portfolio_scheduler.adapters.input.swagger.SwaggerPurchas
 import com.matheus.srv_portfolio_scheduler.application.command.ExecutePortfolioPurchase.ExecutePortfolioPurchaseCommand;
 import com.matheus.srv_portfolio_scheduler.application.command.ExecutePortfolioPurchase.ExecutePortfolioPurchaseResponse;
 import com.matheus.srv_portfolio_scheduler.application.command.ImportQuotes.ImportQuotesCommand;
-import com.matheus.srv_portfolio_scheduler.domain.services.dto.QuoteDTO;
 import com.matheus.srv_portfolio_scheduler.application.ports.input.commands.ExecutePortfolioPurchaseUseCase;
 import com.matheus.srv_portfolio_scheduler.application.ports.input.commands.ImportQuotesUseCase;
+import com.matheus.srv_portfolio_scheduler.domain.services.dto.QuoteDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class PurchasesController implements SwaggerPurchasesController {
     private final ExecutePortfolioPurchaseUseCase executePortfolioPurchaseUseCase;
 
     @PostMapping("execute-purchase")
-    public ResponseEntity<ExecutePortfolioPurchaseResponse> executePurchase(ImportQuotesCommand command) {
+    public ResponseEntity<ExecutePortfolioPurchaseResponse> executePurchase(@RequestBody ImportQuotesCommand command) {
 
         List<QuoteDTO> result = importQuotesUseCase.handler(command);
 
